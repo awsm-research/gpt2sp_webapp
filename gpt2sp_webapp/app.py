d8e2a5242f3ae1746bcd507d2059324630173580
Michael Fu <michaelfu1998@gmail.com>
2021-08-31 13:50:22 +1000

    Update app.py

--- gpt2sp_webapp/app.py
+++ gpt2sp_webapp/app.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
         # set up logo and title
         logo_col, _, title_col = st.columns([10, 6, 70])
         with logo_col:
-            st.image(logo, 150)
+            st.image(logo, width=150)
         with title_col:
             st.title("GPT2SP - Agile Story Point Estimator")
 
