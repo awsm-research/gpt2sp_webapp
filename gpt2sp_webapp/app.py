2e3794dc555c2cf0810be026813a8046e58540b8
Michael Fu <michaelfu1998@gmail.com>
2021-08-31 13:22:09 +1000

    Update app.py

--- gpt2sp_webapp/app.py
+++ gpt2sp_webapp/app.py
@@ -115,7 +115,7 @@ if __name__ == "__main__":
                                         "Usergrid"])
         dataset = MODEL_NAME[dataset]
         dataset = dataset.lower()
-        dataset_path = "./gpt2sp_webapp/historical_data/historical_data/" + dataset + ".csv"
+        dataset_path = "/gpt2sp_webapp/historical_data/historical_data/" + dataset + ".csv"
         st.dataframe(pd.read_csv(dataset_path))
 
     if behavior == "GPT2SP: Agile Story Point Estimator":
